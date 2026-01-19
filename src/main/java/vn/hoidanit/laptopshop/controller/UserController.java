dca5712c1803d7391638c803e8b17948deafe8e1
phamloc2362005 <phamloc2362005@gmail.com>
2026-01-19 17:26:12 +0700

    update

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,41 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+@Controller
+public class UserController {
+
+        //dependency injection
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/")
+    public String getHomePage(){
+        String test = this.userService.handleHello();
+        return "hello";  
+    }
+}
+
+// @RestController
+// public class UserController {
+    
+//     //dependency injection
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+
+//     @GetMapping("")
+//     public String getHomePage(){
+//         return this.userService.handleHello();  
+//     }
+// }
