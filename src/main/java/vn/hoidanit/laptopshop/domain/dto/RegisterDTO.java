cdb290b9b6634321481ab09a443f6e63ac05d2e9
phamloc2362005 <phamloc2362005@gmail.com>
2026-02-07 08:09:20 +0700

    update

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
@@ -0,0 +1,63 @@
+package vn.hoidanit.laptopshop.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
+
+@RegisterChecked
+public class RegisterDTO {
+
+    @Size(min = 3, message = "Fullname phải có tối thiểu 3 kí tự")
+    private String firstName;
+
+    @Size(min = 3, message = "Fullname phải có tối thiểu 3 kí tự")
+    private String lastName;
+
+    @Email(message = "email không hợp lệ", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    private String email;
+
+    private String password;
+
+    @Size(min = 2, message = "Password phải có tối thiểu 2 kí tự")
+    private String confirmPassword;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
