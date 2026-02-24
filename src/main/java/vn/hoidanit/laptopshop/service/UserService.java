47ed96bcf019059839cdf85ab4be5fc18de8241c
phamloc2362005 <phamloc2362005@gmail.com>
2026-02-24 14:33:42 +0700

    update phân trang user

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,6 +2,8 @@ package vn.hoidanit.laptopshop.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.Role;
@@ -28,6 +30,10 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return this.userRepository.findAll();
+    }
+
+    public Page<User> fetchUsers(Pageable pageable) {
+        return this.userRepository.findAll(pageable);
     }
 
     public User getUserById(long id) {
